2fd5a172cee4931d0a74bdb3faf33c377e88a04d
imbarilyn <linahmuhonjaimbari@gmail.com>
2022-10-10 14:56:21 +0300

    added a background image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import heroImage from '../assets/pizza.jpeg'
 
 function Home() {
   const navigate = useNavigate()
   return (
     <div className='home'>
-      <div className='headerContainer'>
+      <div className='headerContainer' style={{backgroundImage: `url(${heroImage})`}} >
         <h1>Imbari's Pizza Point</h1>
         <p>LET'S GIVE YOU A TASTE OF HEAVEN</p>
         <button onClick={()=>navigate("/menu")}>ORDER NOW</button>
