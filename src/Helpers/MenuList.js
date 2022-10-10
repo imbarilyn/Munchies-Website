3cba21ac37eda77341170d0accf1c51be48a8422
imbarilyn <linahmuhonjaimbari@gmail.com>
2022-10-10 19:44:30 +0300

    created a folder to hold list file

---
+++ src/Helpers/MenuList.js
@@ -0,0 +1,41 @@
+import pepperoni from '../assets/pepperoni.jpg';
+import margherita from '../assets/margherita.jpg' ;
+import imbaritechspecial  from '../assets/imbaritechspecial.jpg'
+import vegan from '../assets/vegan.jpg';
+import pineapple from '../assets/pineapple.jpg';
+import expensive from '../assets/expensive.jpg'
+
+const MenuList = [
+    {
+        name: "Pepperoni Pizza",
+        image: pepperoni,
+        price: 14.75
+    },
+    {
+        name: "Margherita",
+        image: margherita,
+        price: 14.93
+    },
+    {
+        name: "Vegan",
+        image: vegan,
+        price: 12.5
+    },
+    {
+        name: "Pineapple",
+        image: pineapple,
+        price: 18.75
+    },
+    {
+        name: "Imbari Special",
+        image: imbaritechspecial,
+        price: 150.75
+    },
+    {
+        name: "Hawaiian",
+        image: expensive,
+        price: 16.45
+    }    
+]
+
+export default MenuList;
