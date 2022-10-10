4e4eda86209e60710596c505879a31e2ac861329
imbarilyn <linahmuhonjaimbari@gmail.com>
2022-10-10 14:47:46 +0300

    created ORDER NOW button with a routing functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const navigate = useNavigate()
   return (
-    <div>Hello world</div>
+    <div className='home'>
+      <div className='headerContainer'>
+        <h1>Imbari's Pizza Point</h1>
+        <p>LET'S GIVE YOU A TASTE OF HEAVEN</p>
+        <button onClick={()=>navigate("/menu")}>ORDER NOW</button>
+      </div>
+    </div>
   )
 }
 
